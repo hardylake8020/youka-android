7b6c4966ebefd72f622fb579e2af43a899a779cf
csbz17027 <77113015@qq.com>
2016-12-24 14:49:30 +0800

    Merge from the mastefr

--- app/src/main/java/com/zzqs/app_kc/z_kc/activity/FindGoodsActivity.java
+++ app/src/main/java/com/zzqs/app_kc/z_kc/activity/FindGoodsActivity.java
@@ -1,10 +1,8 @@
 package com.zzqs.app_kc.z_kc.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,128 +15,126 @@ import com.zzqs.app_kc.z_kc.listener.MyOnClickListener;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Created by lance on 2016/12/4.
  */
 
 public class FindGoodsActivity extends BaseActivity implements XListView.IXListViewListener {
-  TextView tvLeft, tvTitle, tvRight, tvSelectAll, tvSelectBidding, tvSelectGrab;
-  EditText etStart, etEnd;
-  XListView lvTenders;
-  TenderAdapter adapter;
-  private List<Tender> tenderList;
-  private int selectType;
-  private static final int ALL = 1;
-  private static final int BIDDING = 2;
-  private static final int GRAB = 3;
+    TextView tvLeft, tvTitle, tvSelectAll, tvSelectBidding, tvSelectGrab;
+    EditText etStart, etEnd;
+    XListView lvTenders;
+    TenderAdapter adapter;
+    private List<Tender> tenderList;
+    private int selectType;
+    private static final int ALL = 1;
+    private static final int BIDDING = 2;
+    private static final int GRAB = 3;
 
-  @Override
-  public void initVariables() {
-    tenderList = new ArrayList<>();
-  }
-
-  @Override
-  public void initViews(Bundle savedInstanceState) {
-    setContentView(R.layout.z_kc_act_find_goods);
-    tvLeft = (TextView) findViewById(R.id.head_back);
-    tvLeft.setText("");
-    tvLeft.setOnClickListener(new MyOnClickListener() {
-      @Override
-      public void OnceOnClick(View view) {
-        finish();
-      }
-    });
-    tvTitle = (TextView) findViewById(R.id.head_title);
-    tvTitle.setText(R.string.goods_list);
-    tvRight = (TextView) findViewById(R.id.head_right);
-    tvRight.setText(R.string.find_good_by_map);
-    tvRight.setVisibility(View.VISIBLE);
-    tvSelectAll = (TextView) findViewById(R.id.tvSelectAll);
-    tvSelectAll.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        setSelectType(ALL);
-      }
-    });
-    tvSelectAll.setTextColor(ContextCompat.getColor(this, R.color.red));
-    tvSelectBidding = (TextView) findViewById(R.id.tvSelectBidding);
-    tvSelectBidding.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        setSelectType(BIDDING);
-      }
-    });
-    tvSelectGrab = (TextView) findViewById(R.id.tvSelectGrab);
-    tvSelectGrab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        setSelectType(GRAB);
-      }
-    });
-    etStart = (EditText) findViewById(R.id.etStart);
-    etEnd = (EditText) findViewById(R.id.etEnd);
-    lvTenders = (XListView) findViewById(R.id.lvTenders);
-    lvTenders.setPullRefreshEnable(true);
-    lvTenders.setPullLoadEnable(false);
-    lvTenders.setXListViewListener(this);
-    adapter = new TenderAdapter(this, tenderList, false);
-    lvTenders.setAdapter(adapter);
-    lvTenders.stopRefresh();
-    lvTenders.stopLoadMore();
-  }
-
-  @Override
-  public void loadData() {
-
-  }
-
-  @Override
-  public void onRefresh() {
-    getKCOrders();
-    onLoad();
-  }
-
-  @Override
-  public void onLoadMore() {
-    getKCOrders();
-    onLoad();
-  }
-
-  private void getKCOrders() {
-  }
-
-  /**
-   * 停止刷新，
-   */
-  private void onLoad() {
-    lvTenders.stopRefresh();
-    lvTenders.stopLoadMore();
-    lvTenders.setRefreshTime(getString(R.string.xilstview_refreshed));
-    if (tenderList.size() >= 10) {
-      lvTenders.setPullLoadEnable(true);
+    @Override
+    public void initVariables() {
+        tenderList = new ArrayList<>();
     }
-  }
 
-  private void setSelectType(int type) {
-    if (selectType == type) {
-      return;
-    }
-    tvSelectAll.setTextColor(ContextCompat.getColor(this, R.color.click_gray));
-    tvSelectBidding.setTextColor(ContextCompat.getColor(this, R.color.click_gray));
-    tvSelectGrab.setTextColor(ContextCompat.getColor(this, R.color.click_gray));
-    switch (type) {
-      case ALL:
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.z_kc_act_find_goods);
+        tvLeft = (TextView) findViewById(R.id.head_back);
+        tvLeft.setText("");
+        tvLeft.setOnClickListener(new MyOnClickListener() {
+            @Override
+            public void OnceOnClick(View view) {
+                finish();
+            }
+        });
+        tvTitle = (TextView) findViewById(R.id.head_title);
+        tvTitle.setText(R.string.goods_list);
+        tvSelectAll = (TextView) findViewById(R.id.tvSelectAll);
+        tvSelectAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSelectType(ALL);
+            }
+        });
         tvSelectAll.setTextColor(ContextCompat.getColor(this, R.color.red));
-        break;
-      case BIDDING:
-        tvSelectBidding.setTextColor(ContextCompat.getColor(this, R.color.red));
-        break;
-      case GRAB:
-        tvSelectGrab.setTextColor(ContextCompat.getColor(this, R.color.red));
-        break;
+        tvSelectBidding = (TextView) findViewById(R.id.tvSelectBidding);
+        tvSelectBidding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSelectType(BIDDING);
+            }
+        });
+        tvSelectGrab = (TextView) findViewById(R.id.tvSelectGrab);
+        tvSelectGrab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSelectType(GRAB);
+            }
+        });
+        etStart = (EditText) findViewById(R.id.etStart);
+        etEnd = (EditText) findViewById(R.id.etEnd);
+        lvTenders = (XListView) findViewById(R.id.lvTenders);
+        lvTenders.setPullRefreshEnable(true);
+        lvTenders.setPullLoadEnable(false);
+        lvTenders.setXListViewListener(this);
+        adapter = new TenderAdapter(this, tenderList, false);
+        lvTenders.setAdapter(adapter);
+        lvTenders.stopRefresh();
+        lvTenders.stopLoadMore();
     }
-    selectType = type;
 
-    getKCOrders();
-  }
+    @Override
+    public void loadData() {
+    }
+
+    @Override
+    public void onRefresh() {
+        getKCOrders();
+        onLoad();
+    }
+
+
+    @Override
+    public void onLoadMore() {
+        getKCOrders();
+        onLoad();
+    }
+
+    /**
+     * 停止刷新，
+     */
+    private void onLoad() {
+        lvTenders.stopRefresh();
+        lvTenders.stopLoadMore();
+        lvTenders.setRefreshTime(getString(R.string.xilstview_refreshed));
+        if (tenderList.size() >= 10) {
+            lvTenders.setPullLoadEnable(true);
+        }
+    }
+
+    private void getKCOrders() {
+    }
+
+    private void setSelectType(int type) {
+        if (selectType == type) {
+            return;
+        }
+        tvSelectAll.setTextColor(ContextCompat.getColor(this, R.color.click_gray));
+        tvSelectBidding.setTextColor(ContextCompat.getColor(this, R.color.click_gray));
+        tvSelectGrab.setTextColor(ContextCompat.getColor(this, R.color.click_gray));
+        switch (type) {
+            case ALL:
+                tvSelectAll.setTextColor(ContextCompat.getColor(this, R.color.red));
+                break;
+            case BIDDING:
+                tvSelectBidding.setTextColor(ContextCompat.getColor(this, R.color.red));
+                break;
+            case GRAB:
+                tvSelectGrab.setTextColor(ContextCompat.getColor(this, R.color.red));
+                break;
+        }
+        selectType = type;
+
+        getKCOrders();
+    }
 }
--- app/src/main/java/com/zzqs/app_kc/z_kc/activity/MainActivity.java
+++ app/src/main/java/com/zzqs/app_kc/z_kc/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.zzqs.app_kc.z_kc.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
